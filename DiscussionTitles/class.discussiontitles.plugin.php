491ef48d708fad63d68bb443c5ee9949c1059aef
James <james.ducker@gmail.com>
2020-04-16 09:21:18 +1000

    Add plugin

--- DiscussionTitles/class.discussiontitles.plugin.php
+++ DiscussionTitles/class.discussiontitles.plugin.php
@@ -1,24 +1,17 @@
 <?php
-/**
- * @copyright 2009-2019 Vanilla Forums Inc.
- * @license http://www.opensource.org/licenses/gpl-2.0.php GNU GPL v2
- * @package DiscussionTitles
- */
 
-/**
- * Class DiscussionTitlesPlugin
- */
 class DiscussionTitlesPlugin extends Gdn_Plugin {
+    /**
+     * This plugin strips all trailing punctuation from discussion titles to enforce
+     * grammar conventions around titles and make your discussion lists look a bit less
+     * bleak!
+     */
 
-    function stripTrailingPeriods($str) {
+    function stripTrailingPunctuation($str) {
         return preg_replace('/[[:punct:]]+$/', '', $str);
     }
 
     public function discussionModel_beforeSaveDiscussion_handler($sender, $args) {
-        try {
-            $args['FormPostValues']['Name'] = $this->stripTrailingPeriods($args['FormPostValues']['Name']);
-        } catch (Exception $e) {
-            Logger.error($e->getMessage());
-        }
+        $args['FormPostValues']['Name'] = $this->stripTrailingPunctuation($args['FormPostValues']['Name']);
     }
 }
